cd19324f1e757331ba360462044ae7637332b9e7
Pedro <pedrocarvalholima2015@gmail.com>
2026-02-26 19:56:33 -0300

    feat: add main application entry point

---
+++ src/main/java/com/pamp/security/PasswordStrengthAnalyzerApplication.java
@@ -0,0 +1,8 @@
+package com.pamp.security;
+
+public class PasswordStrengthAnalyzerApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Password Strength Analyzer started.");
+    }
+}
